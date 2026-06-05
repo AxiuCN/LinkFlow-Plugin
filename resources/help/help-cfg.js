66179195bf02a9e408a158f9b48d440314766022
阿修Axiu <1194310418@qq.com>
2026-06-06 00:21:21 +0800

    feat: 帮助页启用主题背景图，完善图标索引

--- resources/help/help-cfg.js
+++ resources/help/help-cfg.js
@@ -1,5 +1,5 @@
 export const helpCfg = {
-  title: '#B站帮助',
+  title: '#b站帮助',
   subTitle: 'Bilibili-Plugin 帮助'
 }
 
@@ -7,32 +7,32 @@ export const helpList = [
   {
     group: 'B站账号',
     list: [
-      { icon: 0, title: '#b站登录', desc: '扫码登录B站账号' },
-      { icon: 0, title: '#b站状态', desc: '查看登录状态与Cookie有效期' }
+      { icon: 1, title: '#b站登录', desc: '扫码登录B站账号' },
+      { icon: 80, title: '#b站状态', desc: '查看登录状态' }
     ]
   },
   {
     group: '激励计划',
     list: [
-      { icon: 0, title: '#激励创建配置', desc: '从全局默认生成个人配置' },
-      { icon: 0, title: '#激励添加 <链接>', desc: '向个人配置添加兑换链接' },
-      { icon: 0, title: '#激励列表', desc: '查看个人兑换配置' },
-      { icon: 0, title: '#激励删除 <编号>', desc: '删除指定链接' }
+      { icon: 75, title: '#激励创建配置', desc: '生成个人兑换配置' },
+      { icon: 87, title: '#激励添加 <链接>', desc: '添加兑换链接' },
+      { icon: 80, title: '#激励列表', desc: '查看个人配置' },
+      { icon: 92, title: '#激励删除 <编号>', desc: '删除指定链接' }
     ]
   },
   {
     group: '激励管理（仅主人）',
     auth: 'master',
     list: [
-      { icon: 0, title: '#添加激励白名单', desc: '将QQ添加到激励白名单' },
-      { icon: 0, title: '#删除激励白名单', desc: '从激励白名单移除QQ' },
-      { icon: 0, title: '#激励白名单', desc: '查看激励白名单' }
+      { icon: 31, title: '#添加激励白名单', desc: '添加QQ到白名单' },
+      { icon: 92, title: '#删除激励白名单', desc: '从白名单移除QQ' },
+      { icon: 80, title: '#激励白名单', desc: '查看白名单' }
     ]
   },
   {
     group: '通用',
     list: [
-      { icon: 0, title: '#b站帮助', desc: '查看Bilibili-Plugin帮助图' }
+      { icon: 1, title: '#b站帮助', desc: '查看帮助图' }
     ]
   }
 ]
